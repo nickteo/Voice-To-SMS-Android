276ef5668281dd237dc1bab07d7d907b303fe6ce
unknown <gt224@cornell.edu>
2015-12-23 17:49:57 -0500

    Send transcription to the phone

--- app/src/main/java/com/example/nickteo/voicetosms/MainActivity.java
+++ app/src/main/java/com/example/nickteo/voicetosms/MainActivity.java
@@ -1,32 +1,38 @@
 package com.example.nickteo.voicetosms;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
 import java.util.UUID;
 
 public class MainActivity extends ActionBarActivity {
-    5f8e15dd-acad-4d8b-9f01-1869ef95b57e
 
-    private static final int KEY_BUTTON_UP = 0;
-    private static final int KEY_BUTTON_DOWN = 1;
-    private static final UUID APP_UUID = UUID.fromString("3717c824-d2db-4dea-b977-bcd263dd505e");
+    private static final int KEY_PHONE_NUMBER = 0;
+    private static final int KEY_MESSAGE = 1;
+    private static final UUID APP_UUID = UUID.fromString("5f8e15dd-acad-4d8b-9f01-1869ef95b57e");
 
     private PagerAdapter mPagerAdapter;
 
     private PebbleKit.PebbleDataReceiver mDataReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("onCreate", "onCreate");
     }
 
     @Override
@@ -50,7 +56,8 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    /** */
+
+
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
         EditText phoneNumber = (EditText) findViewById(R.id.phone_number);
@@ -70,4 +77,31 @@ public class MainActivity extends ActionBarActivity {
                     Toast.LENGTH_LONG).show();
         }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("tag", "onResume");
+
+        // Set up data receiver handler
+        if(mDataReceiver == null) {
+            mDataReceiver = new PebbleKit.PebbleDataReceiver(APP_UUID) {
+
+                @Override
+                public void receiveData(Context context, int transactionId, PebbleDictionary dict) {
+                    // Message received, over!
+                    PebbleKit.sendAckToPebble(context, transactionId);
+                    // Grab the transcription
+                    String transcription = dict.getString(KEY_MESSAGE);
+                    Log.i("something", "received message");
+                    if (transcription != null) {
+                        Log.i("receiveData", "Transcription: " + transcription);
+                    }
+                }
+
+            };
+            PebbleKit.registerReceivedDataHandler(getApplicationContext(), mDataReceiver);
+        }
+    }
+
 }
